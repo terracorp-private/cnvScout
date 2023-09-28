43a27bae31a697e3b2db8bfdd096691ab44114d0
terracorp-private <terracorp.private.com>
2023-09-28 13:53:41 +0200

    some scripts copied from .ipynb to .py

--- cnv_forest.py
+++ cnv_forest.py
@@ -2,55 +2,66 @@
 import pandas as pd
 import numpy as np
 
+
 # Modelling
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, ConfusionMatrixDisplay
 from sklearn.model_selection import RandomizedSearchCV, train_test_split
 from scipy.stats import randint
 
-# Tree Visualisation
-from sklearn.tree import export_graphviz
-from IPython.display import Image
-from IPython.display import display
-import graphviz
 
 
-df = pd.read_csv("/home/alpha/programs/python_files/datasets/cnv_and_mut/entities.csv")
+df = pd.read_csv("/home/alpha/programs/python_files/datasets/cnv_and_mut/entities_transformed.csv")
+
+print(df.head())
 
 df = df.dropna(axis=1)
-print(df)
 # df = df[df.columns.drop(list(df.filter(regex='[YX]')))]
-filter = df["Unnamed: 0"].str.contains(r'(^9|^3|^100|^201|^101|^200|^8)')
-df = df[~filter]
-X = df.drop("Unnamed: 0",axis=1)
+X = df.drop("ID",axis=1)
 X = X.drop("entity",axis=1)
-print(X)
 y = df["entity"].map({'gbm_cnv':0,'k27_cnv':1,'mng_cnv':2,'oligo_cnv':3,'pxa_cnv':4,'astroLow_cnv':5,'astroHigh_cnv':6,'pa_cnv':7})
-print(y)
+
+# Standartize data
+X =( X - X.mean() ) / X.std()
 
 # Data splitting
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y)
-
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+print(type(X_train))
 
 # Model fitting
-rf = RandomForestClassifier()
+rf = RandomForestClassifier(max_depth=9,n_estimators=294)
 rf.fit(X_train, y_train)
 
-# Accuracy test
+# Accuracy testprint(y_pred)
 y_pred = rf.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 print("Accuracy:", accuracy)
 
-# Export the first three decision trees from the forest
 
-for i in range(3):
-    tree = rf.estimators_[i]
-    dot_data = export_graphviz(tree,
-                               feature_names=X_train.columns,  
-                               filled=True,  
-                               max_depth=2, 
-                               impurity=False, 
-                               proportion=True)
-    graph = graphviz.Source(dot_data)
-    display(graph)
+# SECTION II
+    
+param_dist = {'n_estimators': randint(50,500),
+              'max_depth': randint(1,20)}
 
+# Create a random forest classifier
+rf = RandomForestClassifier()
+
+# Use random search to find the best hyperparameters
+rand_search = RandomizedSearchCV(rf, 
+                                 param_distributions = param_dist, 
+                                 n_iter=5, 
+                                 cv=5)
+
+# Fit the random search object to the data
+rand_search.fit(X_train, y_train)
+
+# SECTION III
+
+# Create a variable for the best model
+best_rf = rand_search.best_estimator_
+
+# Print the best hyperparameters
+print('Best hyperparameters:',  rand_search.best_params_)
+
+# Generate predictions with the best model
+y_pred = best_rf.predict(X_test)
--- cnv_umap.py
+++ cnv_umap.py
@@ -1,85 +1,174 @@
 import pandas as pd
 import numpy as np
 
-import plotly.express as px
-from umap import UMAP
+
+# preprocessing and dimensionality reduction
 from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import RobustScaler
+from sklearn.preprocessing import QuantileTransformer
+from sklearn.preprocessing import Normalizer
 from sklearn.decomposition import PCA
+import pywt
+from umap import UMAP
 
 
-df = pd.read_csv("/home/alpha/programs/python_files/datasets/cnv_and_mut/entities.csv")
+# visualization
+import matplotlib.pyplot as plt
+import plotly.express as px
 
-# some samples have more columns, filter them out and drop all samples from 450k. Usually theier ID begins with 9 or 3
-df = df.dropna(axis=1)
-# filter = df["Unnamed: 0"].str.contains(r'(^9|^3|^100|^201|^101|^200|^8)')
-# df = df[~filter]
 
-# drop xy chromosomes and make features
+AMPLITUDE_THRESHOLD = 0.06
+
+
+df = pd.read_csv("/home/alpha/programs/python_files/datasets/cnv_and_mut/entities_transformed.csv")
+
+# basic filtering. Rename Unnamded 0 with ID. Dropp chromosomes Y and X
+# df = df.dropna(axis=1)
+# df = df.fillna(0)
+# df = df.rename(columns={"Unnamed: 0": "ID"})
 df = df[df.columns.drop(list(df.filter(regex='[YX]')))]
+
+print(df.head())
+# Make it amplitude agnostic (experimental) 
+def stupid_custom_filter(df=df,AMPLITUDE_THRESHOLD=AMPLITUDE_THRESHOLD):
+    # temporary drop non digit columns
+    helper_df = df[["entity","ID"]]
+    df = df.drop(columns=["entity","ID"])
+    # standardize and normalize data
+    df = (df-df.mean())/df.std()
+    df = 2*((df-df.min())/(df.max()-df.min()))-1
+    # apply the filter
+    df[df < -AMPLITUDE_THRESHOLD] = -1
+    df[df > AMPLITUDE_THRESHOLD] = 1
+    df[(df <= AMPLITUDE_THRESHOLD) & (df >= -AMPLITUDE_THRESHOLD)] = 0
+    # append the dropped columns on the filtered dataframe
+    df["ID"] = helper_df["ID"]
+    df["entity"] = helper_df["entity"]
+    
+    return df
+
+# df = stupid_custom_filter()
+
+# Make features
 features = df.columns.tolist()
 features.remove("entity")
-features.remove("Unnamed: 0")
+features.remove("ID")
 
-# make targets and scale the data
+print(df.head())
+
+
+
+PRINCIPAL_COMPONENTS = 5
+
+# make targets
 x = df.loc[:,features].values
 y_entity = df.loc[:,"entity"].values
-y_id = df.loc[:,"Unnamed: 0"].values
-x = StandardScaler().fit_transform(x)
+y_id = df.loc[:,"ID"].values
 
 
-pca_denoize = PCA(n_components=5)
+# test transform and preprocessing methods
+def custom_preprocess(method=1,x=x):
+    if method == 2:
+        scaler = MinMaxScaler()
+    elif method == 3:
+        scaler = RobustScaler(quantile_range=(0.2,0.8))
+    elif method == 4:
+        scaler = QuantileTransformer(n_quantiles=10, random_state=0, output_distribution="normal")
+    elif method == 5:
+        scaler = Normalizer()
+    else:
+        print("using default StandardScaler")
+        scaler = StandardScaler()
+    
+    x = scaler.fit_transform(x)
+    # x = Normalizer(norm="max").fit_transform(x)
+    return x
+    
+method = int(input("Scaler to use (default StandardSclaer) : \n 1.StandardScaler \n 2.MinMaxScaler \n 3.RobustScaler \n 4.QuantileTransformer \n 5. Normalizer \n"))
+x = custom_preprocess(method,x)
+
+
+# PCA
+pca_denoize = PCA(n_components=PRINCIPAL_COMPONENTS)
 denoize = pca_denoize.fit_transform(x)
-print(denoize.shape)
+# print components which contribute to variance the most
+variance = pca_denoize.explained_variance_ratio_
+noise = pca_denoize.noise_variance_
+print(variance, noise)
 
-umap_2d = UMAP(n_neighbors=7,n_epochs=5000,random_state=1,spread=3)
+plt.figure(figsize=[14,6])
+plt.bar(range(variance.shape[0]),variance)
+plt.xlabel('PCA features')
+plt.ylabel('variance %')
+
+
+N_NEIGHBORS = 6
+
+umap_2d = UMAP(n_neighbors=N_NEIGHBORS,n_epochs=500,random_state=1,spread=3,min_dist=0.5)
 umap_2d.fit(denoize)
 
 projections = umap_2d.transform(denoize)
 
 fig = px.scatter(
     projections, x=0, y=1,
+    title= f'PCA components = {PRINCIPAL_COMPONENTS}, n_neighbors = {N_NEIGHBORS}, n_cases = {df.shape[0]}, fold_change_threshold = {AMPLITUDE_THRESHOLD}' ,
     color=y_entity.astype(str), labels={'color': 'entity'},
-    opacity=0.9
+    opacity=1
 )
+
+fig.update_layout(width=1000, height=600)
+
 fig.show()
 
 
-# # UMAP FOR SEGMENTS
-#
-# df = df[["entity","ID","chr","start","end","alteration","seg_mean","seg_median"]]
-# df["ID"] = df["ID"].str.replace(r"[RC_]","",regex=True)
-# df["methylation_class"] = df["entity"]
-# df["methylation_class"] = df["methylation_class"].replace(df["methylation_class"].unique().tolist(),[0,1,2,3,4])
-# df['id_numeric'] = df.reset_index().index
-# df["alteration"] = df["alteration"].replace(["balanced"],0)
-# df = df[df["alteration"] != 0]
-# df["alteration"] = df["alteration"].replace(["loss"],1)
-# df["alteration"] = df["alteration"].replace(["gain"],2)
-# df["alteration"] = df["alteration"].replace(["chromothripsis"],3)
-# df["length"] = df["end"] - df["start"]
-# df["chr"] = df["chr"].astype("int")
-# df = df.dropna()
-# # df = df.drop_duplicates(subset=["ID"])
-#
-# features = ["id_numeric","ID","chr","alteration","start","end","length","seg_mean","methylation_class"]
-#
-# print(df.tail(10))
-#
+
+
+# SETTINGS FOR .BIN.IGV
+
+
+# import pandas as pd
+# import numpy as np
+
+# import plotly.express as px
+# from umap import UMAP
+# from sklearn.preprocessing import StandardScaler
+# from sklearn.decomposition import PCA
+
+
+# df = pd.read_csv("/home/alpha/programs/python_files/datasets/cnv_and_mut/entities.csv")
+
+# # some samples have more columns, filter them out and drop all samples from 450k. Usually theier ID begins with 9 or 3
+# df = df.dropna(axis=1)
+# # filter = df["Unnamed: 0"].str.contains(r'(^9|^3|^100|^201|^101|^200|^8)')
+# # df = df[~filter]
+
+# # drop xy chromosomes and make features
+# df = df[df.columns.drop(list(df.filter(regex='[YX]')))]
+# features = df.columns.tolist()
+# features.remove("entity")
+# features.remove("Unnamed: 0")
+
+# # make targets and scale the data
 # x = df.loc[:,features].values
 # y_entity = df.loc[:,"entity"].values
-# y = df.loc[:,"chr"].values
+# y_id = df.loc[:,"Unnamed: 0"].values
 # x = StandardScaler().fit_transform(x)
-#
-# print(len(y))
-#
-# umap_2d = UMAP(n_neighbors=25,min_dist=0.2)
-# umap_2d.fit(x)
-#
-# projections = umap_2d.transform(x)
-#
+
+
+# pca_denoize = PCA(n_components=5)
+# denoize = pca_denoize.fit_transform(x)
+# print(denoize.shape)
+
+# umap_2d = UMAP(n_neighbors=7,n_epochs=5000,random_state=1,spread=3)
+# umap_2d.fit(denoize)
+
+# projections = umap_2d.transform(denoize)
+
 # fig = px.scatter(
 #     projections, x=0, y=1,
 #     color=y_entity.astype(str), labels={'color': 'entity'},
-#     opacity=0.7
+#     opacity=0.9
 # )
 # fig.show()
+
