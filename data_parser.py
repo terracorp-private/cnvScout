4f1edabb6338bae8144878710993dce77a743aa9
terracorp-private <terracorp.private@gmail.com>
2025-05-16 07:05:05 +0200

    Better organization of data_parser.py

--- data_parser.py
+++ data_parser.py
@@ -6,22 +6,46 @@ import re
 import pandas as pd
 
 
-def datamatix_constructor(path_to_cnn, output_file):
+DATA_DIR = sys.argv[1]
+
+
+def sample_selector(DATA_DIR):
+    '''Read sample sheet and pick sample names, sex, age and
+    diagnosis. Drop all samples with low read count.'''
+
+    annotation_file = DATA_DIR + "features_frame_annon.xlsx"
+
+    if annotation_file not in glob.glob(DATA_DIR+"*"):
+        raise Exception("ERROR! Annotation file either not found or has wrong name")
+
+    cases_df = pd.read_excel(annotation_file, usecols=["Barcode", "ID", "Diagnose", "Mean Cov"])
+
+    if cases_df.empty:
+        raise Exception(f"ERROR! Annotation file is empty")
+
+    # Set the lowes tolerated read count. Default 200 reads.
+    cases_df = cases_df[cases_df["Mean Cov"] >= 200]
+
+    # Construct a distinct identifier. Since same samples could be
+    # found accross multiple libraries.
+    cases_df["sample"] = cases_df["Barcode"] + "_" + cases_df["ID"]
+
+    if cases_df["sample"].duplicated().sum().sum() != 0:
+        cases_df = cases_df.drop_duplicates(subset=["sample"])
+
+    return cases_df
+
+
+def datamatix_constructor(path_to_cnn, output_file, samples_df):
     '''The function iterates through cnn files and merges them to one data_matrix,
     output needs an argv argument. Use /tmp/tmp.csv if you don't need one
     '''
 
     # TODO Duplicated samples management. Actuall solution is really quick and dirty.
+    # Read sample sheet. Construct barcode_SampleID identifier. Since there could be duplicated samples in separate libraries.
+    # Drop duplicated barcode_SampleIDs and return a sample list.
 
-    cases_df = pd.read_excel("../../../Documents/datasets/cnv_ngs/features_frame_annon.xlsx", usecols=["Barcode", "ID", "Diagnose"])
-    cases_df["sample"] = cases_df["Barcode"] + "_" + cases_df["ID"]
-
-    if cases_df["sample"].duplicated().sum().sum() != 0:
-        cases_df = cases_df.drop_duplicates(subset=["sample"])
-        print("Duplicates were dropped", cases_df[cases_df["sample"].duplicated() == True], sep="\n")
-
-    selected_cases = cases_df["sample"].to_list()
-
+    selected_cases = samples_df["sample"].to_list()
 
     # empty dataframe for final merged data
     data = pd.DataFrame()
@@ -37,34 +61,24 @@ def datamatix_constructor(path_to_cnn, output_file):
             df["sample"] = sample
             data = pd.concat([df,data])
 
-    # print(data.isnull().any().any())
+    # make identifier chrom_genomic_position
     data["identifier"] = data["chromosome"].astype(str) + "_" + data["start"].astype(str)
     data = data[(data["chromosome"] != "chrY") & (data["chromosome"] != "chrX")]
     data = data.drop(["chromosome","start"],axis=1)
 
-    # TODO try nans
-    # print(data.isnull().any().any())
-    # print(data)
-    # print(smpl_ser)
-    # smpl = data[data["sample"] == "N1363_25"]
-    # smpl_ser = data["sample"].value_counts()
-
+    # generate Pivot table merged on sample
     data_long = data.pivot(index="sample", columns="identifier", values="log2")
-    data_long = pd.merge(data_long, cases_df, on="sample", how="inner")
+    data_long = pd.merge(data_long, samples_df, on="sample", how="inner")
     data_long = data_long.drop(columns=["ID", "Barcode"])
-
-    # TODO try nans
-    # print(data_long.isnull().any().any())
 
     # TODO make state of the art output file
     if output_file != "":
-        # print(data_long["sample"].isnull().any().any())
         data_long.to_csv(output_file)
         print(data_long)
     else:
         print("No output")
 
 
-path_to_cnn_directory, output_file = sys.argv[1:]
 
-datamatix_constructor(path_to_cnn_directory, output_file)
+samples_df = sample_selector(DATA_DIR)
+datamatix_constructor(DATA_DIR+"antitarget_2025/",DATA_DIR+"raw_datamatrix.csv", samples_df)
